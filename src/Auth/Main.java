907eb36cae926cf929675bacbe8a740bf48de5e8
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2025-06-25 13:53:21 +0200

    new code

--- src/Auth/Main.java
+++ src/Auth/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/Auth/resource/Login.fxml")));
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/Auth/resource/Homefx.fxml")));
         stage.setTitle("BookCradle | Auth Portal");
         stage.setScene(scene);
         stage.show();
